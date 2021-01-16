2583c122f45dcbe427f3c7e1841fc047e32eba8e
Florian Kretlow <floriankretlow@gmx.de>
2021-01-16 10:59:36 +0100

    initial work on getting things sorted

--- src/ts/cli.ts
+++ src/ts/cli.ts
@@ -1,30 +1,32 @@
-import { CLEngine, WebShell } from "./cl-engine.js";
+interface IStreamListener {
+    notify(what: string): void;
+}
+interface IStream {
+    subscribe(who: IStreamListener): void;
+    unsubscribe(whom: IStreamListener): void;
+}
+interface OStream {
+    push(what: string): void;
+}
 
-class CLInterface {
+class CLInterface implements IStream, OStream {
     private active: Boolean = false;
-    private buffer: string = "";
     private history: string[] = [];
-    private listing: HTMLUListElement;
-    private input: HTMLInputElement;
+    private output: HTMLUListElement = new HTMLUListElement();
+    private input: HTMLInputElement = new HTMLInputElement();
+    private listeners: IStreamListener[] = [];
 
-    constructor(private root: HTMLElement, private engine: CLEngine = new WebShell()) {
+    constructor(private root: HTMLElement) {
         this.root.classList.add("cli-container");
 
-        this.listing = document.createElement("ul");
-        this.listing.classList.add("cli-listing");
-        this.root.appendChild(this.listing);
+        // this.output = document.createElement("ul");
+        this.output.classList.add("cli-output");
+        this.root.appendChild(this.output);
 
-        this.input = document.createElement("input");
+        // this.input = document.createElement("input");
         this.input.classList.add("cli-input");
         this.root.appendChild(this.input);
         this.input.value = this.engine.prompt();
-
-        this.input.addEventListener("keyup", (e) => {
-            if (e.code === "Enter") {
-                this.commit(this.input.value);
-                this.input.value = this.engine.prompt();
-            }
-        });
 
         /* Trap tab. */
         this.input.addEventListener("keydown", (e) => {
@@ -34,63 +36,43 @@ class CLInterface {
             }
         })
 
-        this.input.addEventListener("input", (e) => {
-            if (this.input.value.length < this.engine.prompt().length) {
-                this.input.value = this.engine.prompt();
-            }
-        });
-
-        this.input.addEventListener("blur", () => {
-            this.active = false;
-        })
-
-        this.root.addEventListener("click", (e) => {
-            e.stopPropagation();
-            this.activate();
-        });
-
-        window.addEventListener("click", () => {
-            if (this.active) this.deactivate();
-        });
-
-        this.record(...this.engine.initiate());
+        this.input.addEventListener("input", e => { this.commit(e); });
+        this.input.addEventListener("blur", e => { this.deactivate(e); });
+        this.root.addEventListener("click", e => { this.activate(e); });
+        window.addEventListener("click", () => { if (this.active) this.deactivate(); });
     }
 
-    activate() {
+    activate(e: Event): void {
         if (!this.active) {
             this.active = true;
             this.input.value = this.engine.prompt();
             this.input.focus();
         }
+        e.stopPropagation();
     }
 
-    deactivate() {
+    deactivate(e: Event): void {
         if (this.active) {
             this.active = false;
             this.input.blur();
         }
     }
 
-    update() {
-        this.listing.textContent = "";
+    update(): void {
+        this.output.textContent = "";
         for (const line of this.history) {
             let li: HTMLElement = document.createElement("li");
-            li.classList.add("cli-listing-line");
+            li.classList.add("cli-output-line");
             li.textContent = line;
-            this.listing.appendChild(li);
+            this.output.appendChild(li);
         }
     }
 
-    record(...lines: string[]) {
-        for (const line of lines) {
+    push(what: string): void {
+        for (const line of what.split("\n")) {
             this.history.push(line);
         }
         this.update();
-    }
-
-    commit(line: string) {
-        this.record(line);
-        this.record(...this.engine.process(line.slice(this.engine.prompt().length)));
     }
 }
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,5 @@
 /* TS usually omits the file ending, but this is needed for the browser */
-import { CLInterface } from "./cli.js"; 
+import { CLInterface } from "./cl-interface.js"; 
 
 window.addEventListener("load", () => {
     const r: HTMLElement | null = document.querySelector("#cli");
